91efbea25cc04269576d293728d3b7717d0b5270
YS.VostroPC <rotace2012227ys@gmail.com>
2016-09-02 20:57:05 +0900

    modify octave/work/solve_inequality

--- octave_sample/work/solve_inequality/main.m
+++ octave_sample/work/solve_inequality/main.m
@@ -1,45 +1,44 @@
 % main.m
 
-a.org=[0,0];
-b.org=[20,20];
+a.org=[0,0,0];
+b.org=[20,20,0];
 
-a.dif=[1,1];
-b.dif=[1,1];
+a.dif=[1,1,0];
+b.dif=[1,1,0];
 
-a.itrx=2;
-a.itry=3;
+a.itrx=4;
+a.itry=4;
 b.itrx=4;
-b.itry=5;
+b.itry=4;
 
-pax=zeros(a.itrx,a.itry);
-pay=zeros(a.itrx,a.itry);
-pbx=zeros(b.itrx,b.itry);
-pby=zeros(b.itrx,b.itry);
-res=zeros(a.itrx,a.itry,b.itrx,b.itry);
+pax=zeros(a.itrx,a.itry,b.itrx,b.itry);
+pay=zeros(a.itrx,a.itry,b.itrx,b.itry);
+pbx=zeros(a.itrx,a.itry,b.itrx,b.itry);
+pby=zeros(a.itrx,a.itry,b.itrx,b.itry);
+res=zeros(a.itrx,a.itry,b.itrx,b.itry)==1;
 
 for iax=1:a.itrx
     for iay=1:a.itry
 	for ibx=1:b.itrx
 	    for iby=1:b.itry
 		
-		va=a.org+a.dif.*[iax,iay];
-		vb=b.org+b.dif.*[ibx,iby];
-		pax(iax,iay)=va(1);
-		pay(iax,iay)=va(2);
-		pbx(ibx,iby)=vb(1);
-		pby(ibx,iby)=vb(2);
-
+		va=a.org+a.dif.*[iax-1,iay-1,0];
+		vb=b.org+b.dif.*[ibx-1,iby-1,0];
 
 		tmp=true;
 
-		if abs(va)<3
+		if norm(va)<1.1
 		  tmp=false;
 		end
 
-		if abs(vb)>22
+		if norm(vb)>30
 		  tmp=false;
 		end
-
+		
+		pax(iax,iay,ibx,iby)=va(1);
+		pay(iax,iay,ibx,iby)=va(2);
+		pbx(iax,iay,ibx,iby)=vb(1);
+		pby(iax,iay,ibx,iby)=vb(2);
 		res(iax,iay,ibx,iby)=tmp;
 		
 	    end
@@ -47,32 +46,28 @@ for iax=1:a.itrx
     end
 end
 
-pax=reshape(pax,[a.itrx*a.itry,1]);
-pay=reshape(pay,[a.itrx*a.itry,1]);
-nsola=zeros(a.itrx,a.itry);
-for iax=1:a.itrx
-    for iay=1:a.itry
-      nsola(iax,iay)=sum(sum(res(iax,iay,:,:)));
-    end
-end
-nsola=reshape(nsola,[a.itrx*a.itry,1]);
+nsola=sum(sum(res,3),4);
+nsolb=sum(sum(res,1),2);
+x=[pax(:,:,1,1)(:);pbx(1,1,:,:)(:)];
+y=[pay(:,:,1,1)(:);pby(1,1,:,:)(:)];
+scale=[nsola(:);nsolb(:)].*[255]./max([nsola(:);nsolb(:)]);
+scatter(x,y,scale/10+5,scale,'filled');
 
-pbx=reshape(pbx,[b.itrx*b.itry,1]);
-pby=reshape(pby,[b.itrx*b.itry,1]);
-nsolb=zeros(b.itrx,b.itry);
-for ibx=1:b.itrx
-    for iby=1:b.itry
-      nsolb(ibx,iby)=sum(sum(res(:,:,ibx,iby)));
-    end
-end
-nsolb=reshape(nsolb,[b.itrx*b.itry,1]);
-
-scatter([pax;pbx],[pay;pby],[],[nsola;nsolb].*[255,0,0]./max([nsola;nsolb]));
 
 res=permute(res,[1,3,2,4]);
 res=reshape(res,[a.itrx*b.itrx,a.itry*b.itry]);
+res=res';
+
 figure;
-spygray(res)
+pcolor([res,zeros(a.itry*b.itry,1);zeros(1,a.itrx*b.itrx+1)]);
+colorbar;
+axis square;
 
+figure;
+res=flipud(res);
+imagesc(res);
+colorbar;
+axis square;
 
-
+data=[pax(res),pay(res),pbx(res),pby(res)];
+plotmatrix(data);
