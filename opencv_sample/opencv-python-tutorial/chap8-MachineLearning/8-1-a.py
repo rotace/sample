74aadfe093df855e545fb98df65356eace73f872
YS.VostroPC <rotace2012227ys@gmail.com>
2016-11-30 23:34:11 +0900

    add opencv_sample chap8

---
+++ opencv_sample/opencv-python-tutorial/chap8-MachineLearning/8-1-a.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+trainData = np.random.randint(0, 100, (25, 2)).astype(np.float32)
+responses = np.random.randint(0, 2, (25, 1)).astype(np.float32)
+
+red = trainData[responses.ravel() == 0]
+plt.scatter(red[:, 0], red[:, 1], 80, 'r', '^')
+
+blue = trainData[responses.ravel() == 1]
+plt.scatter(blue[:, 0], blue[:, 1], 80, 'b', 's')
+
+newcomer = np.random.randint(0, 100, (10, 2)).astype(np.float32)
+plt.scatter(newcomer[:, 0], newcomer[:, 1], 80, 'g', 'o')
+
+knn = cv2.KNearest()
+knn.train(trainData, responses)
+ret, results, neighbours, dist = knn.find_nearest(newcomer, 3)
+
+print "result: ", results, "\n"
+print "neighbours: ", neighbours, "\n"
+print "distance:", dist
+
+plt.show()
